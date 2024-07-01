b43e4b45602282849574b3c2f4f7a41711d54cf1
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-07-01 00:26:02 -0300

    update

---
+++ pages/Relatorios.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+      <GanhosGrafico /> 
+    </v-container>
+  </template>
+  
+  <script>
+  import GanhosGrafico from '~/components/GanhosGraficos.vue';
+  
+  export default {
+    components: {
+      GanhosGrafico,
+      // ... outros componentes
+    },
+    // ... outros métodos
+  };
+  </script>
