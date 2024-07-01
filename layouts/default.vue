fad463fd372e401217f1727aa88cdd76be11e011
Lucas-Salomao <lucastadeusalomao@gmail.com>
2024-07-01 00:13:22 -0300

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,20 @@
 <template>
-    <v-app>
-      <Navbar />
-      <v-main>
-        <v-container>
-          <Nuxt />
-        </v-container>
-      </v-main>
-    </v-app>
-  </template>
-  
-  <script>
-  import Navbar from "../components/Navbar.vue"
-  
-  export default {
-    components: {
-      Navbar,
-    },
-  };
-  </script>
-  
+  <v-app>
+    <Navbar />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
