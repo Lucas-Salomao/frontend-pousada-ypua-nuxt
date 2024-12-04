4904c82f4cb38615ab18cd17a5dc32953ada651d
Lucas Salomao <lucastadeusalomao@gmail.com>
2024-12-04 14:36:18 -0300

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,28 @@
-export default function ({ $auth, redirect }) {
-  console.log($auth.loggedIn)
-  if (!$auth.loggedIn) {
-    return redirect('/login')
+// export default function ({ $auth, redirect }) {
+//   console.log($auth.loggedIn)
+//   if (!$auth.loggedIn) {
+//     return redirect('/login')
+//   }
+// }
+
+import routes from '../routes';
+
+export default function ({ $auth, route, redirect }) {
+  // Remove o redirecionamento para /login
+
+  if ($auth.loggedIn) { // Verifica se o usuário está logado
+    const userRole = $auth.user.role; 
+
+    if (!checkAccess(userRole, route.path)) {
+      return redirect('/'); 
+    }
   }
+};
+
+function checkAccess(role, routePath) {
+  const routeConfig = routes.find(route => route.path === routePath);
+  if (routeConfig) {
+    return routeConfig.roles.includes(role);
+  }
+  return false; // Se a rota não estiver configurada, nega o acesso por padrão
 }
